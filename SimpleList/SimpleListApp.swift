e83d9e4bb7640dc552115eed7ebe7d908eab6262
Std1o <37378410+Std1o@users.noreply.github.com>
2021-03-27 18:48:54 +0300

    Initial commit

---
+++ SimpleList/SimpleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListApp.swift
+//  SimpleList
+//
+//  Created by Илья on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
